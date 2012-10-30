ad1296def81688fdcc6b0d6eedd8111d44274bd2
johann <git@johann-hofmann.com>
2012-10-30 09:21:19 +0100

    Point3 erledigt

--- src/Point3.java
+++ src/Point3.java
@@ -1,9 +1,9 @@
 
 public class Point3 {
 	
-	double x;
-	double y;
-	double z;
+	final double x;
+	final double y;
+	final double z;
 	
 	public Point3(double x, double y, double z) {
 		super();
@@ -13,13 +13,14 @@ public class Point3 {
 	}
 	
 	public Vector3 sub (Point3 p){
-		
+		return new Vector3(x - p.x,y-p.y,z-p.z);
 	}
 	
 	public Point3 sub (Vector3 v){
+		return new Point3(x - v.x,y-v.y,z-v.z)
 		
 	}
 	public Point3 add (Vector3 v){
-		
+		return new Point3(x + v.x,y+v.y,z+v.z)
 	}
 }
