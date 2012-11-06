bf314c591623eba9d18fb1a56cfc43ea955c62b0
Krebs2012 <anton_beuthhochschule@hotmail.de>
2012-11-06 17:25:51 +0100

    /Code-Style javadoc

--- src/Point3.java
+++ src/Point3.java
@@ -3,24 +3,44 @@ public class Point3 {
 	final double x;
 	final double y;
 	final double z;
-
+	/**
+	 * 
+	 * @param x x-value for a 3 dimensional system
+	 * @param y y-value for a 3 dimensional system
+	 * @param z z-value for a 3 dimensional system
+	 */
 	public Point3(final double x, final double y, final double z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
+		/**
+		 * Point 3 gives us a point with our values x,y,z
+		 */
 	}
 
 	public final Vector3 sub(Point3 p) {
 		return new Vector3(x - p.x, y - p.y, z - p.z);
+		/**
+		 *  Vecotor3 sub(Point3 p) gives us the result of a subtraction 
+		 *  between a vector and a point
+		 */
 	}
 
 	public final Point3 sub(Vector3 v) {
 		return new Point3(x - v.x, y - v.y, z - v.z);
+		/**
+		 *  Point3 sub(Vector3 v) gives us the result of a subtraction
+		 *  between a point and a vector
+		 */
 
 	}
 
 	public final Point3 add(Vector3 v) {
 		return new Point3(x + v.x, y + v.y, z + v.z);
+		/**
+		 * Point3 add(Vector3 v) gives us the result of a addition
+		 * between a point and a vector
+		 */
 	}
 
 	@Override
